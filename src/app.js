c10e4a7ab520bb78630e78e27bb0db4faa607bec
vrod <rodrigues.victor.vsr@gmail.com>
2016-06-10 01:28:52 -0300

    Introduce new types of animation elements and add Lesson01 (tentative)
    Introduce two new types of animation elements: CircleElement and SimpleGridElement
    Add tentative for lesson 01 with basic shapes and concepts

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var DummyLesson = require("./lesson/dummy_lesson");
+var Lesson01 = require("./lesson/lesson01");
 var LessonEnvironment = require("./view/lesson_environment");
 
 var startLesson = function(lessonClass, domElement) {
@@ -15,4 +16,5 @@ var startLesson = function(lessonClass, domElement) {
 module.exports = {
   startLesson: startLesson,
   DummyLesson: DummyLesson,
+  Lesson01: Lesson01,
 };
